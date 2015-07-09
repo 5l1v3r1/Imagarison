2aca9961b3997acc9bef88053b6f8184a590045a
torque59 <helofrancis@gmail.com>
2015-07-09 10:04:58 +0530

    Install file for node-resemble (ubuntu)

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "[+] For installing node-resemble"
+sudo apt-get install libgif-dev
+sudo apt-get install libcairo2-dev
+sudo apt-get install libpango1.0-dev
+
+sudo apt-get install git
+ 
+echo "[+] Now go to Imagarison Folder and do npm install node-resemble"
