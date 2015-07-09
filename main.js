1fc49dc751e863d32f746bb883d535944513b2ed
torque59 <helofrancis@gmail.com>
2015-07-09 10:07:05 +0530

    Added resemeble-js some probs need to fix

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-var fs=require('fs');
+
+
+var fs = require('fs');
 var http = require('http');
+var resemble = require('node-resemble');
 
 function download(url, dest, callback) {
     var file = fs.createWriteStream(dest);
@@ -20,12 +23,27 @@ url=['http://reviewmything.com/wp-content/uploads/Never-Regret-Something-Faceboo
 
 console.log("Downloading the url's");
 
+var filenames = []
+
 for(i=0;i<2;i++)
 {
 	
 	console.log("Downloading " + url[0]);
 	download(url[i],'/home/opensec/Desktop/Imagarison/'+i+url[i].slice(-4));//Change Path to Ur Windows/Linux Full Path
+	filenames.push('/home/opensec/Desktop/Imagarison/'+i+url[i].slice(-4));
 }
 
 console.log("Download Completed");
 
+var img1 = fs.readFileSync('./0.png');
+var img2 = fs.readFileSync('./1.png');
+
+
+// Not executing the resemble (check)
+
+var diff = resemble(img1).compareTo(img2).ignoreColors().onComplete(function(data)
+{
+    //console.log("h");
+    console.log(data);
+});
+console.log(diff);
