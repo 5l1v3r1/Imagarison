00d99ca3002ffc115acca1594587a00aa4eb6d16
torque59 <helofrancis@gmail.com>
2015-07-15 01:14:12 +0530

    Added install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 npm install -g request
+echo "[+] For installing node-resemble"
+sudo apt-get install libgif-dev
+sudo apt-get install libcairo2-dev
+sudo apt-get install libpango1.0-dev
+
+sudo apt-get install git
+ 
+echo "[+] Now go to Imagarison Folder and do npm install node-resemble"
+
