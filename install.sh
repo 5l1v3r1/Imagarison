36f00955a0e947e4cbd7b85c2377069e47c93b7e
Francis Alexander <helofrancis@gmail.com>
2019-07-19 21:40:54 +0530

    test commit

--- install.sh
+++
@@ -1,9 +0,0 @@
-echo "[+] For installing node-resemble"
-sudo apt-get install libgif-dev
-sudo apt-get install libcairo2-dev
-sudo apt-get install libpango1.0-dev
-sudo apt-get install libjpeg-dev
-sudo apt-get install git
- 
-echo "[+] Now go to Imagarison Folder and do npm install node-resemble"
-
